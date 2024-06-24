eb8514adce7504496e7f09679ee4b08a676f0b8e
galRubel <127147440+galRubel@users.noreply.github.com>
2024-06-24 11:18:42 -0300

    fix(deploy): Uvicorn default host

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 
 
+from fastapi import FastAPI
+import uvicorn
 from typing import Union
 
-from fastapi import FastAPI
 
 app = FastAPI()
 
-if __name__ == "__main__":
-    port =4000
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
-
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
