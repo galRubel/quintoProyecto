df828fb65addb8e447a3aa582f532e519158148d
Galu1905 <galrubel19@gmail.com>
2024-07-03 20:01:22 -0300

    fix: Emprolije todo

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    return {"Msg": "World"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+#@app.get("/items/{item_id}")
+#async def read_item(item_id: int, q: Union[str, None] = None):
+#    return {"item_id": item_id, "q": q}
 
